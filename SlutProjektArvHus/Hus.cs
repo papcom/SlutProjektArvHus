cffdc104bfe44d15ad2936517437bcae4cbf99ca
pajon <pajon@PHILIPS-PC>
2023-05-07 01:29:35 +0200

    Add project files.

---
+++ SlutProjektArvHus/Hus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjektArvHus
+{
+    class Hus
+    {
+        public string Färg { get; set; }
+        public Hus(string färg)
+        {
+            Färg = färg;
+        }
+        public void SkrivFärg()
+        {
+            Console.WriteLine("Detta är ett " + Färg + " hus.");
+        }
+    }
+    class RöttHus : Hus
+    {
+        public RöttHus() : base("rött") { }
+    }
+    class BlåttHus : Hus
+    {
+        public BlåttHus() : base("blått") { }
+    }
+}
